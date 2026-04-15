e5ca854bbb054edea4bf9befd5d3d77ef8e439fc
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2026-04-15 09:29:36 -0300

    Add content creators data and include script in index.html

---
+++ assets/js/data/content_creators_data.js
@@ -0,0 +1,20 @@
+const contentCreatorsData = [
+    {
+        name: 'Junior Martins',
+        role: 'Engenheiro Mobile',
+        image: 'assets/images/junior.jpg',
+        linkedin: 'https://www.linkedin.com/in/jrmartins/',
+    },
+    {
+        name: 'Gabriel Moro',
+        role: 'Engenheiro Mobile',
+        image: 'assets/images/moro.jpg',
+        linkedin: 'https://www.linkedin.com/in/gabrielbronzattimoro15031994/',
+    },
+    {
+        name: 'Pedro Alvarez',
+        role: 'Engenheiro Mobile',
+        image: 'assets/images/pedro.jpg',
+        linkedin: 'https://www.linkedin.com/in/pedro-alvarez94/',
+    }
+]
